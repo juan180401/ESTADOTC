da6326d6228333136f4d11deea794b797a0677aa
Wolf <jachpera2015@gmail.com>
2026-09-05 16:59:18 -0600

    feat: feat: agregar entidades para cards y transactions

---
+++ ESTADOTC.API/Domain/Entities/Card.cs
@@ -0,0 +1,20 @@
+﻿namespace ESTADOTC.API.Domain.Entities
+
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        public string CardNumber { get; set; } = string.Empty;
+
+        public string HolderName { get; set; } = string.Empty;
+
+        public decimal CreditLimit { get; set; }
+
+        public decimal InterestRate { get; set; }
+
+        public decimal MinimumPaymentRate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
