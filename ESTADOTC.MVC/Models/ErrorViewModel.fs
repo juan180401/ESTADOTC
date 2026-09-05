3ea4a378817de0244a3a2a2465f7e2b2fdb9f8ab
Wolf <jachpera2015@gmail.com>
2026-09-05 15:50:51 -0600

    feat: Agregar Preyectos base API y MVC

---
+++ ESTADOTC.MVC/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace ESTADOTC.MVC.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
