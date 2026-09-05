6247abaf08a372b580dfd6e96c66d7366a2e0c5b
Wolf <jachpera2015@gmail.com>
2026-09-05 17:35:39 -0600

    feat: implementar acceso a datos con Dapper

---
+++ ESTADOTC.API/Infrastructure/Repositories/CardRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using Dapper;
+using ESTADOTC.API.Domain.Entities;
+using ESTADOTC.API.Infrastructure.Data;
+using ESTADOTC.API.Infrastructure.Repositories.Interfaces;
+using System.Data;
+
+namespace ESTADOTC.API.Infrastructure.Repositories;
+
+public class CardRepository : ICardRepository
+{
+    private readonly IDbConnectionFactory _connectionFactory;
+
+    public CardRepository(IDbConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    public async Task<Card?> GetCardStatementAsync(int cardId)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+
+        var parameters = new
+        {
+            CardId = cardId
+        };
+
+        return await connection.QueryFirstOrDefaultAsync<Card>(
+            "dbo.sp_GetCardStatement",
+            parameters,
+            commandType: CommandType.StoredProcedure);
+    }
+}
---
+++ ESTADOTC.API/Infrastructure/Repositories/Interfaces/ICardRepository.cs
@@ -0,0 +1,8 @@
+﻿using ESTADOTC.API.Domain.Entities;
+
+namespace ESTADOTC.API.Infrastructure.Repositories.Interfaces;
+
+public interface ICardRepository
+{
+    Task<Card?> GetCardStatementAsync(int cardId);
+}
