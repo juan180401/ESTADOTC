0dacfaa513470e1d47a8e38fb9c772f0ebd69239
Wolf <jachpera2015@gmail.com>
2026-09-06 17:36:02 -0600

    feat: implementar controller de tarjetas en MVC

---
+++ ESTADOTC.MVC/Controllers/CardController.fs
@@ -0,0 +1,13 @@
+﻿namespace ESTADOTC.MVC.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open ESTADOTC.MVC.Services
+
+type CardController(apiService: ICardApiService) =
+    inherit Controller()
+
+    member this.Index() =
+        task {
+            let! card = apiService.GetCardStatementAsync(1)
+            return this.View(card)
+        }
