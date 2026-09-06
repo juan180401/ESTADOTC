10b867940e000b29b15d1ee2ef768a33c7be3644
Wolf <jachpera2015@gmail.com>
2026-09-06 16:33:43 -0600

    feat: implementar validacion automatica con FluentValidation

--- ESTADOTC.API/Program.cs
+++ ESTADOTC.API/Program.cs
@@ -6,6 +6,7 @@ using ESTADOTC.API.Infrastructure.Repositories;
 using ESTADOTC.API.Infrastructure.Repositories.Interfaces;
 using ESTADOTC.API.Infrastructure.UnitOfWork;
 using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,7 @@ builder.Services.AddValidatorsFromAssemblyContaining<AddPurchaseDtoValidator>();
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
