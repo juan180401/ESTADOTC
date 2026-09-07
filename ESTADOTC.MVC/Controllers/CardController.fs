2a6591290d070d901881a8ef247e9c3d3634da68
Wolf <jachpera2015@gmail.com>
2026-09-06 18:12:46 -0600

    feat: implementar registro de compras en MVC

--- ESTADOTC.MVC/Controllers/CardController.fs
+++ ESTADOTC.MVC/Controllers/CardController.fs
@@ -1,5 +1,7 @@
 ﻿namespace ESTADOTC.MVC.Controllers
 
+open System
+open System.Threading.Tasks
 open Microsoft.AspNetCore.Mvc
 open ESTADOTC.MVC.Services
 open ESTADOTC.MVC.Models
@@ -32,4 +34,56 @@ type CardController(apiService: ICardApiService) =
                 }
 
             return this.View(model)
+        }
+
+    [<HttpGet>]
+    member this.Purchase() =
+        let model =
+            {
+                TransactionDate = DateTime.Today
+                Description = ""
+                Amount = 0M
+            }
+
+        this.View(model)
+
+    [<HttpPost>]
+    [<ValidateAntiForgeryToken>]
+    member this.Purchase(
+        transactionDate: DateTime,
+        description: string,
+        amount: decimal
+    ) : Task<IActionResult> =
+        task {
+            if String.IsNullOrWhiteSpace(description) then
+                this.ModelState.AddModelError(
+                    "Description",
+                    "La descripción es obligatoria."
+                )
+
+            if amount <= 0M then
+                this.ModelState.AddModelError(
+                    "Amount",
+                    "El monto debe ser mayor que cero."
+                )
+
+            if not this.ModelState.IsValid then
+                let model =
+                    {
+                        TransactionDate = transactionDate
+                        Description = description
+                        Amount = amount
+                    }
+
+                return this.View(model) :> IActionResult
+            else
+                do!
+                    apiService.AddPurchaseAsync(
+                        1,
+                        transactionDate,
+                        description,
+                        amount
+                    )
+
+                return this.RedirectToAction("Index") :> IActionResult
         }
